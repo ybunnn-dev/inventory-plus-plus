19b4e2e8148b0839ca4461c5acae53c6f3613b26
John Ivan Belaro <belaroivan17@gmail.com>
2026-08-26 10:52:39 +0800

    added other modules

--- resources/js/components/sidebar.vue
+++ resources/js/components/sidebar.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="w-[300px] min-h-[calc(100vh-1.5rem)] border-r border-r-gray-300 rounded-l-xl bg-white px-5 py-10 flex flex-col">
-        <h1 class="font-bold text-3xl text-gray-700">Inventory<span class="text-blue-700">++</span></h1>
+        <h1 class="font-bold text-3xl text-gray-700">
+            <span class="text-blue-700">Inve</span>ntory<span class="text-blue-700">++</span>
+        </h1>
 
         <div class="mt-10">
+            <p class="text-xs text-gray-500 font-semibold mb-4">INVENTORY++ MENU</p>
             <ul class="">
                 <li>Dashboard</li>
                 <li>Point on Sales</li>
